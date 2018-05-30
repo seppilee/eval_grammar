3a96717a8a17c3483a8961d8f4e9708271b38c47
cuba <seppi.lee@hanmail.net>
2018-05-30 11:29:01 +0900

    random extracted

--- get_random_line.py
+++ get_random_line.py
@@ -4,7 +4,9 @@
 import random
 import sys
 
+extracted_lines = 700
+
 lines = []
 for line in sys.stdin:
     lines.append(line)
-print(" ".join(str(s) for s in random.sample(lines, 700)))
+print(" ".join(str(s) for s in random.sample(lines, extracted_lines)))
