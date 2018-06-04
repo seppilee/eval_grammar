89892bc7a7bb5d79faa11f82fd8becc79204f849
cuba <seppi.lee@hanmail.net>
2018-06-04 10:51:32 +0900

    remove diacritics

--- normalize.py
+++ normalize.py
@@ -5,16 +5,19 @@ import unicodedata
 import sys
 import re
 
-
 #utf-8 encoding for python2
 reload(sys)
-sys.setdefaultencoding("UTF-8")
 
-diacritics = ur'[\u0301\u030a\u030c]'
+sys.setdefaultencoding("UTF-8")
+#diacritics = ur'[\u0301\u030a\u030c]'
+diacritics = re.compile(ur"([\u0300-\u036F])")
+
 
 for line in sys.stdin:
     line_uni = unicode(line, "utf-8").rstrip()
-    normalized_line = unicodedata.normalize('NFD', line_uni)
-    no_diacritics_line = re.sub(diacritics, '', normalized_line)
+    line = unicodedata.normalize('NFD', line_uni)
+    line = line.replace(u"«", u"“").replace(u"»", u"”")
+    normalized_line = line.encode('utf8').sub(r'(^|[^S\w])#([A-Za-z0-9_]+)', '\\1｟#\\2｠')
+    no_diacritics_line = diacritis.sub('', normalized_line)
     line_utf8 = no_diacritics_line.encode("utf-8")
     print line_utf8
