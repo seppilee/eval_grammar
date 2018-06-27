f15a0b3bbd82a41afa2a9e6206e2f25b342e2ed2
cuba <seppi.lee@hanmail.net>
2018-06-27 14:17:27 +0900

    - length proportion

--- char_length.py
+++ char_length.py
@@ -1,11 +1,9 @@
+
 #!/usr/bin/python
 #-*- coding: UTF-8 -*-
-
 import sys
 import re
-import codecs
-from itertools import izip
-import glob
+import collections
 
 #utf-8 encoding for python2
 reload(sys)
@@ -32,5 +30,33 @@ for line in sys.stdin:
         print "processing:", line_num
     #print line, "\t", char_length
 
-print "total lines:", line_num, "\tmean length:", sum_char_length/line_num, "\tmax length:", max(char_lengths)
-print "long sentences:",len([x for x in char_lengths if x >= threshold])
+c = collections.Counter()
+c.update(char_lengths)
+#long_sent = [c[el] for el in c.elements() if el >= 100 and el <150]
+
+long_sent1 = len([x for x in char_lengths if x <= 50])
+long_sent2 = len([x for x in char_lengths if x > 50 and x <= 100])
+long_sent3 = len([x for x in char_lengths if x > 100 and x <= 150])
+long_sent4 = len([x for x in char_lengths if x > threshold])
+
+print ''.rjust(10), "***************"
+print ''.rjust(10), "Length Summary"
+print ''.rjust(10), "***************"
+
+print "total lines:", line_num, "\tmean length:", sum_char_length/line_num, \
+      "\tmax length:", max(char_lengths), "\tlong sentence(>",threshold,"):",long_sent4
+
+print ""
+print ''.rjust(10), "***************"
+print ''.rjust(10), 'Most common(1-5):'
+print ''.rjust(10), "***************"
+for letter, count in c.most_common(5):
+    print 'chars: %s, count: %3d' % (letter, count)
+
+print ""
+print ''.rjust(10), "********************"
+print ''.rjust(10), "proportion of length"
+print ''.rjust(10), "********************"
+print "chars( 1 - 50):", long_sent1, "(", 100 * float(long_sent1)/float(line_num), "%)"
+print "chars(50 - 100):", long_sent2, "(", 100 * float(long_sent2)/float(line_num), "%)"
+print "chars(100- 150):", long_sent3, "(", 100 * float(long_sent3)/float(line_num), "%)"
